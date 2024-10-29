8676194351b6ec630aeeae498187e73711f0907f
devtracer <devoft1@gmail.com>
2024-10-30 00:46:55 +0330

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,117 +1,70 @@
 #!/bin/bash
 
-# Ensure the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit 1
-fi
-
-# Function to list available disks
-list_disks() {
-    echo "Available disks:"
-    lsblk -d -n -p | awk '{print $1, $2}'
+# Function to print a formatted message
+function print_info() {
+  echo -e "\n\033[1;32m$1\033[0m"
 }
 
-# Function to create partitions
-create_partitions() {
-    local disk="$1"
-
-    # Create partitions: Root (50G), Swap (8G), and EFI (200M)
-    echo "Creating partitions on $disk..."
-
-    # Clear the disk (optional, uncomment if needed)
-    # wipefs -a "$disk"
-
-    # Create partitions using fdisk
-    (
-        echo g # Create a new GPT partition table
-        echo n # New partition for root
-        echo 1 # Partition number 1
-        echo   # Default first sector
-        echo +50G # Size of root partition
-        echo n # New partition for swap
-        echo 2 # Partition number 2
-        echo   # Default first sector
-        echo +8G # Size of swap partition
-        echo n # New partition for EFI
-        echo 3 # Partition number 3
-        echo   # Default first sector
-        echo +200M # Size of EFI partition
-        echo t # Change partition type
-        echo 2 # Partition number 2 (swap)
-        echo 19 # Linux swap
-        echo t # Change partition type
-        echo 3 # Partition number 3 (EFI)
-        echo 1 # EFI System
-        echo w # Write changes
-    ) | fdisk "$disk"
-
-    # Format the partitions
-    mkfs.ext4 "${disk}1" # Root partition
-    mkswap "${disk}2" # Swap partition
-    mkfs.fat -F32 "${disk}3" # EFI partition
-
-    # Enable swap
-    swapon "${disk}2"
+# Function to prompt for user input
+function prompt_user() {
+  read -p "$1: " input
+  echo "$input"
 }
 
-# Variables
-HOSTNAME="archlinux"
-TIMEZONE="UTC"
-USERNAME="user"
-PASSWORD="password" # Change this to a secure password
-MOUNT_POINT="/mnt"
+# Function to prompt for password securely
+function prompt_password() {
+  dialog --passwordbox "$1" 10 60 2> password.txt
+  PASSWORD=$(<password.txt)
+  rm password.txt
+}
 
-# List available disks
-list_disks
-echo
-read -p "Enter the disk to install Arch Linux (e.g., /dev/sda): " SELECTED_DISK
+# Partitioning
+print_info "Partitioning the disk..."
+# ... (use sfdisk or a graphical partitioning tool)
 
-# Create partitions
-create_partitions "$SELECTED_DISK"
+# Formatting
+print_info "Formatting partitions..."
+# ... (use mkfs.ext4, mkswap, etc.)
 
-# Mount the root partition
-mount "${SELECTED_DISK}1" "$MOUNT_POINT"
-mkdir -p "$MOUNT_POINT/boot/efi"
-mount "${SELECTED_DISK}3" "$MOUNT_POINT/boot/efi"
+# Mounting
+print_info "Mounting partitions..."
+# ... (use mount and swapon)
 
-# Install necessary packages
-pacstrap $MOUNT_POINT base linux linux-firmware vim
+# Pacman Configuration
+print_info "Configuring package manager..."
+# ... (edit mirrorlist, etc.)
 
-# Generate fstab
-genfstab -U $MOUNT_POINT >> $MOUNT_POINT/etc/fstab
+# Package Installation
+print_info "Installing base system..."
+pacstrap /mnt base base-devel
 
-# Chroot into the new system
-arch-chroot $MOUNT_POINT /bin/bash << EOF
-# Set the timezone
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-hwclock --systohc
+# Chroot
+print_info "Entering chroot environment..."
+arch-chroot /mnt
 
-# Set the hostname
-echo $HOSTNAME > /etc/hostname
+# Configuration
+print_info "Configuring system..."
+# ... (set timezone, locale, keyboard layout, etc.)
 
-# Set locale (uncomment your locale)
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# Set up the root password
-echo "Set root password:"
-passwd
-
-# Create a new user
-useradd -m -G wheel $USERNAME
+# User Creation
+print_info "Creating user account..."
+USERNAME=$(prompt_user "Username")
+prompt_password "Enter password for $USERNAME"
 echo "$USERNAME:$PASSWORD" | chpasswd
+usermod -aG wheel "$USERNAME"
 
-# Configure sudo
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+# Network Configuration
+print_info "Configuring network..."
+# ... (edit network configuration files)
 
-# Install bootloader (for UEFI systems)
-grub-install --target=x86_64-efi --efi-directory=$MOUNT_POINT/boot/efi --bootloader-id=GRUB
+# Boot Loader Installation
+print_info "Installing boot loader..."
+grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-EOF
+# Exit Chroot
+print_info "Exiting chroot environment..."
+exit
 
-# Unmount partitions and reboot
-umount -R $MOUNT_POINT
-echo "Installation complete. You can now reboot."
+# Inform user to reboot manually
+print_info "Installation complete. Please reboot the system manually."
