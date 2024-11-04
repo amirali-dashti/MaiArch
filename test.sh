29c5f7e9f320fc56363a8c69ce9b1e9dbeffb57f
devtracer <devoft1@gmail.com>
2024-11-04 20:07:01 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Update package lists
-sudo pacman -Syu
+# Update package lists and install dialog
+sudo pacman -Sy dialog
+
+# Create a user account
+read -p "Enter username: " username
+read -s -p "Enter password: " password
+
+sudo useradd -m "$username"
+sudo passwd "$username" <<< "$password"
 
 # Install Xorg and a desktop environment (e.g., GNOME)
 sudo pacman -S xorg-server gnome
 
 # Configure network settings (example)
 sudo nano /etc/network/interfaces
-
-# Set up user accounts
-sudo useradd -m newuser
-sudo passwd newuser
 
 # Start the display server and desktop environment
 sudo systemctl start display-manager
